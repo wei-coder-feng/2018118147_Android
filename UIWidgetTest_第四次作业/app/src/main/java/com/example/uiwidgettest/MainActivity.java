a8fbe23634c68439867acce48acde49bbc806a5a
weijiafeng <1696851773@qq.com>
2020-10-20 13:04:13 +0800

    作业

---
+++ UIWidgetTest_第四次作业/app/src/main/java/com/example/uiwidgettest/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.uiwidgettest;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        final EditText editText = findViewById(R.id.edit_text);
+        final ImageView imageView = findViewById(R.id.image_view);
+        final ProgressBar progressBar = findViewById(R.id.progress_bar);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setTitle("This is ProgressDialog");
+                progressDialog.setMessage("Loading...");
+                progressDialog.setCancelable(true);
+                progressDialog.show();
+            }
+        });
+    }
+}
---
+++ androidWork_第五次作业/RecyclerView.md
@@ -0,0 +1,5 @@
+![image-20201020123127020](C:\Users\16868\AppData\Roaming\Typora\typora-user-images\image-20201020123127020.png)
+
+![image-20201020123442233](C:\Users\16868\AppData\Roaming\Typora\typora-user-images\image-20201020123442233.png)
+
+![image-20201020123745824](C:\Users\16868\AppData\Roaming\Typora\typora-user-images\image-20201020123745824.png)
