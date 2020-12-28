06114b6580df14417698fcf57ecd76aa4cfe1451
weijiafeng <1696851773@qq.com>
2020-12-28 12:57:18 +0800

    作业

---
+++ ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.servicetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private MyService.DownloadBinder downloadBinder;
+    private ServiceConnection connection=new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            downloadBinder = (MyService.DownloadBinder) iBinder;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button startService = (Button) findViewById(R.id.bind_service);
+        Button stopService = (Button) findViewById(R.id.unbind_service);
+        startService.setOnClickListener(this);
+        stopService.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bind_service:
+                Intent intent = new Intent(this, MyService.class);
+                bindService(intent, connection, BIND_AUTO_CREATE);
+                break;
+            case R.id.unbind_service:
+                unbindService(connection);
+                break;
+            default:
+                break;
+        }
+    }
+}
